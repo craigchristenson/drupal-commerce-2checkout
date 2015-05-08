fe64f0e8c1cde71b0db0cf2d7968191129053984
Joaquin Bravo Contreras <jackbravo@gmail.com>
2015-05-07 22:24:19 -0400

    Provide a useful example for the .api.php file

--- commerce_2checkout/commerce_2checkout.api.php
+++ commerce_2checkout/commerce_2checkout.api.php
@@ -17,5 +17,12 @@
  * @see commerce_2checkout_purchase_parameters()
  */
 function hook_commerce_2checkout_order_form_data_alter(&$data, $order) {
-  unset($data['currency_code']);
+  $USD_BOB = (float) variable_get('currency_usd_2_bob', 6.97);
+  $data['currency_code'] = 'USD';
+
+  $i = 0;
+  foreach ($order->commerce_line_items[LANGUAGE_NONE] as $li) {
+    $data['li_' . $i . '_price'] = number_format((float) $data['li_' . $i . '_price'] / $USD_BOB, 2, '.', '');
+    $i++;
+  }
 }
